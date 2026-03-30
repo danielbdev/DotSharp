2455d6de345859716fdccadd10fa13673ebcd207
Daniel Bejar <danielbejar.dev@gmail.com>
2026-03-29 21:34:13 -0300

    feat(persistence.efcore): add Providers constants

---
+++ src/Persistence/DotSharp.Persistence.EFCore/Constants/Providers.cs
@@ -0,0 +1,22 @@
+namespace DotSharp.Persistence.EFCore.Constants;
+
+/// <summary>
+/// Database provider name constants.
+/// </summary>
+public static class Providers
+{
+    /// <summary>
+    /// Microsoft SQL Server provider name.
+    /// </summary>
+    public const string SqlServer = "SqlServer";
+
+    /// <summary>
+    /// PostgreSQL provider name.
+    /// </summary>
+    public const string PostgreSql = "PostgreSql";
+
+    /// <summary>
+    /// SQLite provider name.
+    /// </summary>
+    public const string Sqlite = "Sqlite";
+}
